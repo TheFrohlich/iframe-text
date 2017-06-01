30d4013f64d79f7d7603d2c92db8b5dbd7512538
theFrohlich <theFrohlich@gmail.com>
2017-06-01 16:05:25 +0300

    servers added

---
+++ host/server/devServer.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
+
+
+var app = express();
+
+const YOUR_STATIC_DIR = 'C:/Projects/Tytocare/iframe-text/';
+const YOUR_INDEX_HTM_FILE = 'C:/Projects/Tytocare/iframe-text/host/client/index.html';
+app.use(express.static(YOUR_STATIC_DIR))
+app.get('/', function (req, res) {
+  res.sendFile(YOUR_INDEX_HTM_FILE);
+});
+
+
+var key = fs.readFileSync('./server.key');
+var cert = fs.readFileSync('./server.crt');
+
+console.log(key.toString());
+console.log(cert.toString());
+
+
+
+var options = {
+  key: key,
+  cert: cert,
+  rejectUnauthorized: false,
+  requestCert: true,
+  agent: false
+}
+
+
+var httpServer = http.createServer(app);
+const port = 4000;
+httpServer.listen(port, function () {
+  console.log(`http Server is listening on ${port}`);
+});
+
+
+
+var httpsServer = https.createServer(options, app);
+const port_s = 4001;
+httpsServer.listen(port_s, function () {
+  console.log(`https server is listening on ${port_s}`)
+});
+
